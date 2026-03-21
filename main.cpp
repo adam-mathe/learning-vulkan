5d1913d356d06bdb7b75870947a1ce9279e7124a
Adam <yellows.afterlife@gmail.com>
2026-03-21 11:33:10 -0700

    Graphics pipeline and frame buffers implemeneted

--- main.cpp
+++ main.cpp
@@ -13,6 +13,43 @@
 #include <limits>
 #include <algorithm>
 
+#include <fstream>
+
+//load shaders
+static std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary); //read as binary from the end of the file
+
+    if(!file.is_open()) {
+        throw std::runtime_error("Failed to open file");
+    }
+
+    size_t file_size = (size_t) file.tellg();
+    std::vector<char> buffer(file_size);
+
+    file.seekg(0);
+    file.read(buffer.data(), file_size);
+
+    file.close();
+
+    return buffer;
+}
+
+VkShaderModule makeShaderModule(VkDevice& device, const std::vector<char>& code) {
+    VkShaderModuleCreateInfo create_info;
+
+    create_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    create_info.codeSize = code.size();
+    create_info.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+    VkShaderModule module;
+
+    if(vkCreateShaderModule(device, &create_info, nullptr, &module) != VK_SUCCESS) {
+        throw std::runtime_error("Could not create shader module");
+    }
+
+    return module;
+}
+
 int main() {
     glfwInit();
 
@@ -400,8 +437,255 @@ int main() {
         }
     }
 
+    //now that we have shaders in the spv type, we need to load them
+    auto vert_shader_code = readFile("vert.spv");
+    auto frag_shader_code = readFile("frag.spv");
+
+    //to pass this shader code to vulkan, we need to wrap it in a module
+    VkShaderModule vert_shader_module = makeShaderModule(device, vert_shader_code);
+    VkShaderModule frag_shader_module = makeShaderModule(device, frag_shader_code);
+
+    //now to use these shaders we have to assign them to a certain stage of the rendering pipeline
+    VkPipelineShaderStageCreateInfo vert_shader_stage_info{};
+
+    vert_shader_stage_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vert_shader_stage_info.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vert_shader_stage_info.module = vert_shader_module;
+    vert_shader_stage_info.pName = "main";
+
+    VkPipelineShaderStageCreateInfo frag_shader_stage_info{};
+
+    frag_shader_stage_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    frag_shader_stage_info.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    frag_shader_stage_info.module = frag_shader_module;
+    frag_shader_stage_info.pName = "main";
+
+    //put these stages into an array
+    VkPipelineShaderStageCreateInfo shader_stages[] = {vert_shader_stage_info, frag_shader_stage_info};
+
+    //dynamic states allow us to change specific settings without rebuilding the ENTIRE graphics pipeline
+    std::vector<VkDynamicState> dynamic_states = {
+        VK_DYNAMIC_STATE_VIEWPORT,
+        VK_DYNAMIC_STATE_SCISSOR
+    };
+
+    VkPipelineDynamicStateCreateInfo dynamic_state{};
+
+    dynamic_state.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+    dynamic_state.dynamicStateCount = static_cast<uint32_t>(dynamic_states.size());
+    dynamic_state.pDynamicStates = dynamic_states.data();
+    //now we can change that data at drawing time later
+
+    //for everything else in the pipeline we will use fixed settings
+    VkPipelineVertexInputStateCreateInfo vertex_input_info{};
+
+    vertex_input_info.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertex_input_info.vertexBindingDescriptionCount = 0;
+    vertex_input_info.pVertexBindingDescriptions = nullptr;
+    vertex_input_info.vertexAttributeDescriptionCount = 0;
+    vertex_input_info.pVertexAttributeDescriptions = nullptr;
+    //our vertices are coming from the shader, not the cpu
+
+    //now we need to tell the pipeline how to treat the vertices
+    VkPipelineInputAssemblyStateCreateInfo input_assembly{};
+
+    input_assembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    input_assembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST; //every 3 vertices is 1 triangle
+    input_assembly.primitiveRestartEnable = VK_FALSE;
+
+    //we need these for drawing later
+    VkViewport viewport{};
+
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = (float) extent.width;
+    viewport.height = (float) extent.height;
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+
+    VkRect2D scissor{};
+
+    //we want the scissor rectangle to cover the entire framebuffer
+    scissor.offset = {0,0};
+    scissor.extent = extent;
+    //
+
+    VkPipelineViewportStateCreateInfo viewport_state{};
+
+    viewport_state.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewport_state.viewportCount = 1;
+    viewport_state.pViewports = nullptr; //using dynamic state
+    viewport_state.scissorCount = 1;
+    viewport_state.pScissors = nullptr; //using dynamic state
+
+    //Rasterizer
+    VkPipelineRasterizationStateCreateInfo rasterizer{};
+
+    rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterizer.depthClampEnable = VK_FALSE;
+    rasterizer.rasterizerDiscardEnable = VK_FALSE; //we want to draw
+    rasterizer.polygonMode = VK_POLYGON_MODE_LINE; //solid, line, or point
+    rasterizer.lineWidth = 1.0f; //only matters if drawing lines
+    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT; //if facing away from camera, dont draw
+    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+
+    //not needed for a triangle
+    rasterizer.depthBiasEnable = VK_FALSE;
+    rasterizer.depthBiasConstantFactor = 0.0f;
+    rasterizer.depthBiasClamp = 0.0f;
+    rasterizer.depthBiasSlopeFactor = 0.0f;
+
+    //multisampling or antialisaing. not going to do this for the first triangle
+    VkPipelineMultisampleStateCreateInfo multisampling{};
+
+    multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisampling.sampleShadingEnable = VK_FALSE; //disable it
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+    //color blending, i will skip this for now
+    VkPipelineColorBlendAttachmentState color_blend_attachment{};
+
+    color_blend_attachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    color_blend_attachment.blendEnable = VK_FALSE;
+
+    VkPipelineColorBlendStateCreateInfo color_blending{};
+
+    color_blending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    color_blending.logicOpEnable = VK_FALSE;
+    color_blending.attachmentCount = 1;
+    color_blending.pAttachments = &color_blend_attachment;
+
+    //now make the pipeline layout
+    VkPipelineLayout pipeline_layout{};
+
+    VkPipelineLayoutCreateInfo pipeline_layout_info{};
+
+    pipeline_layout_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    //nothing for now
+
+    if(vkCreatePipelineLayout(device, &pipeline_layout_info, nullptr, &pipeline_layout) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create pipeline layout");
+    }
+
+    //tell the gpu what we are drawing and how to handle the memory
+    VkAttachmentDescription color_attachment{};
+
+    color_attachment.format = surface_format.format;
+    color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+
+    color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    //a render pass can have multiple sub passes
+    VkAttachmentReference color_attachment_ref{};
+
+    color_attachment_ref.attachment = 0;
+    color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass{};
+
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &color_attachment_ref;
+
+    //now we can make the render pass
+    VkRenderPass render_pass;
+    
+    VkRenderPassCreateInfo render_pass_info{};
+
+    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    render_pass_info.attachmentCount = 1;
+    render_pass_info.pAttachments = &color_attachment;
+    render_pass_info.subpassCount = 1;
+    render_pass_info.pSubpasses = &subpass;
+
+    //make the render pass
+    if(vkCreateRenderPass(device, &render_pass_info, nullptr, &render_pass) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create render pass");
+    }
+
+    //finally we can make the pipeline
+    VkGraphicsPipelineCreateInfo pipeline_info{};
+
+    pipeline_info.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipeline_info.stageCount = 2;
+    pipeline_info.pStages = shader_stages;
+
+    //all the settings we defined
+    pipeline_info.pVertexInputState = &vertex_input_info;
+    pipeline_info.pDynamicState = &dynamic_state;
+    pipeline_info.pRasterizationState = &rasterizer;
+    pipeline_info.pMultisampleState = &multisampling;
+    pipeline_info.pViewportState = &viewport_state;
+    pipeline_info.pInputAssemblyState = &input_assembly;
+    pipeline_info.pColorBlendState = &color_blending;
+    pipeline_info.pDepthStencilState = nullptr;
+
+    pipeline_info.layout = pipeline_layout;
+    pipeline_info.renderPass = render_pass;
+    pipeline_info.subpass = 0;
+
+    VkPipeline graphics_pipeline;
+
+    //make the pipeline
+    if(vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipeline_info, nullptr, &graphics_pipeline) != VK_SUCCESS) {
+        throw std::runtime_error("Could not create the graphics pipeline");
+    }
+
+    //we have set up our render pass to expect a single format buffer, we need to set it up
+    //the frame buffer links the render pass to the swap chain images
+
+    std::vector<VkFramebuffer> swapchain_frame_buffers;
+
+    swapchain_frame_buffers.resize(swapchain_imageviews.size());
+
+    for(size_t i = 0; i < swapchain_imageviews.size(); ++i) {
+        VkImageView attachments[] = {
+            swapchain_imageviews[i]
+        };
+
+        VkFramebufferCreateInfo framebuffer_info{};
+
+        framebuffer_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        framebuffer_info.renderPass = render_pass;
+        framebuffer_info.attachmentCount = 1;
+        framebuffer_info.pAttachments = attachments;
+        framebuffer_info.width = extent.width;
+        framebuffer_info.height = extent.height;
+        framebuffer_info.layers = 1;
+
+
+        if(vkCreateFramebuffer(device, &framebuffer_info, nullptr, &swapchain_frame_buffers[i]) != VK_SUCCESS) {
+            throw std::runtime_error("Could not create frame buffer");
+        }
+    }
+
+    //we need a command pool for command buffers
+    VkCommandPool command_pool;
+
+    VkCommandPoolCreateInfo pool_info{};
+
+    pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    pool_info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    pool_info.queueFamilyIndex = indices.graphics_family.value();
+
+    if(vkCreateCommandPool(device, &pool_info, nullptr, &command_pool) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create the command pool");
+    }
+    
+    
+
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+    }
+
+    vkDestroyCommandPool(device, command_pool, nullptr);
+
+    //destroy the frame buffers we created for each image view
+    for(auto framebuffer : swapchain_frame_buffers) {
+        vkDestroyFramebuffer(device, framebuffer, nullptr);
     }
 
     //destroy all the image views we created
@@ -409,6 +693,11 @@ int main() {
         vkDestroyImageView(device, image_view, nullptr);
     }
 
+    vkDestroyPipeline(device, graphics_pipeline, nullptr);
+    vkDestroyPipelineLayout(device, pipeline_layout, nullptr);
+    vkDestroyRenderPass(device, render_pass, nullptr);
+    vkDestroyShaderModule(device, vert_shader_module, nullptr);
+    vkDestroyShaderModule(device, frag_shader_module, nullptr);
     vkDestroySwapchainKHR(device, swapchain, nullptr);
     vkDestroyDevice(device, nullptr);
     vkDestroySurfaceKHR(instance, surface, nullptr);
